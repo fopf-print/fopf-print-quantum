f6770e71f3c55d8a614ad0906382977a784065bc
Данила Манаков <manakov.danila@phystech.edu>
2024-01-30 00:02:18 +0000

    теперь проходят тесты

--- src/quantum/bot.py
+++ src/quantum/bot.py
@@ -8,4 +8,4 @@ fopf_print_bot = Dispatcher()
 @fopf_print_bot.message(CommandStart())
 async def command_start_handler(message: types.Message) -> None:
     """Отвечаем на /start"""
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")  # type: ignore
