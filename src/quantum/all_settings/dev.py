67e1b327eee04e2303f3d159079d9351112cab95
cat-magician <darya2001k@gmail.com>
2024-02-18 00:52:54 +0300

    added balance refill buttons

--- src/quantum/all_settings/dev.py
+++ src/quantum/all_settings/dev.py
@@ -3,4 +3,4 @@ from .prod import *  # noqa
 YOOKASSA_SHOP_ID = '310974'
 YOOKASSA_API_KEY = 'test_0pkffmu0d78Jy1dkewpq-L2hw5WyVSDmCCnBcLmtMks'
 
-FOPF_PRINT_BOT_TOKEN = ''
+FOPF_PRINT_BOT_TOKEN = '6584351083:AAEzj1bhJPTwBnnmMsA4bsBs1J9G2CDMYys'
