b761a4ba9458c2bd2d1770fd12a3fc91bae9721d
Данила Манаков <manakov.danila@phystech.edu>
2024-03-08 23:35:11 +0000

    поправил uri для прода

--- src/quantum/all_settings/prod.py
+++ src/quantum/all_settings/prod.py
@@ -1,3 +1,5 @@
 YOOKASSA_API_ENDPOINT = 'https://api.yookassa.ru/v3/'
 
 FOPF_PRINT_BOT_TOKEN = '6584351083:AAEzj1bhJPTwBnnmMsA4bsBs1J9G2CDMYys'
+
+POSTGRES_CONNECTION_URI = 'postgresql://fopf:vdAKsiEZDKr2akjVD9mi@127.0.0.1:5432/print'
